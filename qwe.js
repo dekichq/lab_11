b5423477db9bb6ce48d017e492b54b2458470d5f
dekichq <fod3ki@gmail.com>
2023-04-11 19:19:27 +0300

    first commit

---
+++ qwe.js
@@ -0,0 +1,74 @@
+// console.log("Кузик"); //Завдання 2
+
+
+// //Завдання 3
+
+// var a = 2;
+// var b = 3;
+
+// console.log("a = ", a);
+// console.log("b = ", b);
+
+// a = b;
+
+// console.log("a = ", a);
+// console.log("b = ", b);
+
+// //Завдання 4
+// const name = "qwe";
+// const age = 0;
+// let valid = true;
+// valid = undefined;
+
+
+
+
+// const user = {
+//     name: "Vasil",
+//     age: 123,
+//     valid: undefined,
+// }
+// console.log(user);
+
+// //Завдання 5
+
+// let isValid = confirm("Чи вам менше 18 років?");
+// alert(isValid);
+
+// // Завдання 6
+// const firstname = "Володимир";
+// const lastname = "Кузик";
+// const group = "КН-321";
+// const bornyear = 2005;
+// var isSingle = true;    
+
+// console.log(bornyear);
+// console.log(firstname);
+// console.log(lastname);
+// console.log(group);
+// console.log(isSingle);
+
+
+// //Завдання 7
+// let login = prompt("Enter login: ");
+// let password = prompt("Enter password: ");
+// let username = prompt("Enter username: ");
+
+// console.log("Dear ", username, " your login is ", login, " and your password is ", password);
+
+
+// //Завдання 8
+// var secinhour = 60*60;
+// var secinday = secinhour * 24;
+// var secinmonth = secinday * 31;
+
+// console.log("Seconds in hour: ", secinhour);
+// console.log("Seconds in day: ", secinday);
+// console.log("Seconds in month: ", secinmonth);
+
+
+
+
+
+
+
